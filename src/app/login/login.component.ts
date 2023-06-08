4266740a444273c57e32d5a5cdaac57e84c5e1ca
CarmineC93 <carmine.caglia93@gmail.com>
2023-06-08 18:04:12 +0200

    add service Authapp for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthappService } from 'src/services/authapp.service';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +16,7 @@ export class LoginComponent implements OnInit {
   okMsg: string = "Ok, you can enter"
   title : string ="Access & Authentication";
 
-  constructor(private route: Router) { }
+  constructor(private route: Router, private BasicAuth: AuthappService) { }
 
   ngOnInit(): void {
   }
@@ -23,7 +24,7 @@ export class LoginComponent implements OnInit {
   gestAuth = () : void => {
     console.log(this.userId);
 
-    if(this.userId === "Carmine" && this.password === "pass"){
+    if(this.BasicAuth.autentica(this.userId, this.password)){
       this.route.navigate(['welcome', this.userId])
       this.autenticated = true;
     }else{
